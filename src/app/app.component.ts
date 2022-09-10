0ab6fec8b1fa4e15aa939405167bd913e1ee55d9
AKq <hnoofk14@gmail.com>
2022-09-10 19:01:55 +0300

    me calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +6,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calc';
+  toshow='0';
+  currvalue ='';
+
+  equals(){
+    this.toshow = eval(this.currvalue)
+    this.currvalue = this.toshow
+  };
+
+  writetoinput(value:string){
+    this.currvalue = this.currvalue + value
+    this.toshow = this.currvalue
+
+  }
+
+  clear(){
+    this.currvalue =''
+    this.toshow = '0'
+  }
+
+
+  back(){
+    this.currvalue = this.currvalue.slice(0,-1)
+    this.toshow = this.currvalue 
+    if(this.toshow =='')
+    {
+      this.toshow = '0'
+    }
+   
+  }
 }
